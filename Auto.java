c70c23d8b1c071c874ebab8dc7d34788190f0965
EstebanSottovia <estebanfsott@gmail.com>
2024-09-04 13:11:37 -0300

    Clase Auto creada

---
+++ Auto.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Auto{
+	public static void main(String[] args){
+		int a,b;
+		int suma;
+		Scanner entrada=new Scanner(System.in);
+		
+		System.out.println("Ingresa dos numeros: ");
+		a=entrada.nextInt();
+		b=entrada.nextInt();
+		
+		suma=a+b;
+		System.out.printf("\nLa suma de los numeros es: %d\n",suma);
+		
+	}
+}
